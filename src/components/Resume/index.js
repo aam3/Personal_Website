4554bce1454390d7755e3909f0bf559f8107c74b
aam3 <aam3@alumni.princeton.edu>
2024-09-13 17:58:30 -0400

    moving photo position

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,8 +8,9 @@ return(
     <div className='container resume-page'>
         <div className="text-zone">
         <h1>RESUME</h1>
-        {/* <h1>'Hello'</h1> */}
-            <iframe src={resumePDF} title='myresume' width='100%' height='800px' />
+            <div className="pdf-zone">
+                <iframe src={resumePDF} title='myresume' width='100%' height='800px' />
+            </div>
         </div>
     </div>
 )
