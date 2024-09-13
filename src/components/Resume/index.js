531b79e250675ce0345295d1e2693463bc0beaf1
aam3 <aam3@alumni.princeton.edu>
2024-09-13 19:58:56 -0400

    adding claude's pdf link suggestions

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,11 @@ const Resume = () => {
 return(
     <div className='container resume-page'>
         <div className="text-zone">
-        <h1><a href="https://drive.google.com/file/d/1e-o9vkuFVgUJ2jsh55TizoVy8LwgRC_1/view?usp=drive_link">RESUME</a></h1>
+        <h1><a 
+        href="https://drive.google.com/file/d/1e-o9vkuFVgUJ2jsh55TizoVy8LwgRC_1/view?usp=drive_link"
+        target="_blank" 
+        rel="noopener noreferrer"        
+        >RESUME</a></h1>
             <div className="pdf-zone">
                 <iframe src={resumePDF} title='myresume' width='100%' height='800px' />
             </div>
