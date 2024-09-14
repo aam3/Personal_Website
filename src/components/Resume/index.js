401aa1b2f9726f7157a96ea3cfb2e3956f23ea01
aam3 <aam3@alumni.princeton.edu>
2024-09-13 20:12:42 -0400

    adjusting z-index for h1 tags

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,15 +4,12 @@ import resumePDF from '../../assets/docs/AMarsteller_Resume_2024.pdf'
 
 const Resume = () => {
 
-    // const googleDriveLink = "https://drive.google.com/file/d/1e-o9vkuFVgUJ2jsh55TizoVy8LwgRC_1/view?usp=drive_link";
-    const directDownloadLink = "https://drive.google.com/uc?export=download&id=1e-o9vkuFVgUJ2jsh55TizoVy8LwgRC_1";
-      
 
 return(
     <div className='container resume-page'>
         <div className="text-zone">
         <h1><a 
-        href={directDownloadLink}
+        href={resumePDF}
         target="_blank" 
         rel="noopener noreferrer"
         >RESUME</a></h1>
