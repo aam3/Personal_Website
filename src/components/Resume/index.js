85e2ba8fb7f4128b0b0d225f0ba4d65484883536
aam3 <aam3@alumni.princeton.edu>
2024-09-13 20:04:49 -0400

    trying google drive download link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,14 +4,17 @@ import resumePDF from '../../assets/docs/AMarsteller_Resume_2024.pdf'
 
 const Resume = () => {
 
+    // const googleDriveLink = "https://drive.google.com/file/d/1e-o9vkuFVgUJ2jsh55TizoVy8LwgRC_1/view?usp=drive_link";
+    const directDownloadLink = "https://drive.google.com/uc?export=download&id=1e-o9vkuFVgUJ2jsh55TizoVy8LwgRC_1";
+      
+
 return(
     <div className='container resume-page'>
         <div className="text-zone">
         <h1><a 
-        href={resumePDF}
+        href={directDownloadLink}
         target="_blank" 
         rel="noopener noreferrer"
-        download="AMarsteller_Resume_2024.pdf"       
         >RESUME</a></h1>
             <div className="pdf-zone">
                 <iframe src={resumePDF} title='myresume' width='100%' height='800px' />
