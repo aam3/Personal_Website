2f79d8b019936abc5c9eda8e968529259339dc1d
aam3 <aam3@alumni.princeton.edu>
2024-09-17 17:05:44 -0400

    moved hamburger icon back to sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,27 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/'
 import './index.scss'
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faBars,
-} from '@fortawesome/free-solid-svg-icons'
 
 const Layout = () => {
-
-  const [showNav, setShowNav] = useState(false);
   
   return (
     <div className="App">
-      <div className={`nav-container ${showNav ? 'mobile-show' : ''}`}>
-        <Sidebar />
-      </div>
-      <FontAwesomeIcon 
-          onClick={() => setShowNav(true)}
-          icon={faBars}
-          color="#181818"
-          size="3x"
-          className='hamburger-icon' />
+      <Sidebar />
       <div className="page">
         <Outlet />
       </div>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import { useState } from 'react'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  // faBars,
+  faBars,
   faClose,
 } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
@@ -14,9 +14,9 @@ const Sidebar = () => {
   const [showNav, setShowNav] = useState(false);
 
   return (
-    <div className={`nav-bar ${showNav ? 'mobile-show' : ''}`}>
+    <div className="nav-bar">
       <nav>
-        <div className="nav-content">
+        <div className={`nav-content ${showNav ? 'mobile-show' : ''}`}>
           <Link to='/' className="logo-link" onClick={() => setShowNav(false)}>
           <img className='signature-logo' src={LogoS} alt='logo'/>
           </Link>
@@ -43,6 +43,12 @@ const Sidebar = () => {
             size="3x"
             className='close-icon' />             
       </nav>
+      <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />      
     </div>
   )
 }
