07f05eb7016c50011dbb02d8220dcdcf8d3af4fe
aam3 <amarsteller95@gmail.com>
2024-10-19 18:48:21 -0400

    change background of expanded menu to dark

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -16,7 +16,7 @@ const Sidebar = () => {
       {/* <Container > */}
         <Navbar.Brand href="/"><img className='signature-logo' src={LogoS} alt='logo'/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" bg="dark">
           <Nav className="nav-content d-flex">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
