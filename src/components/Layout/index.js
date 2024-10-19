acdbce9e90a8cc3bdc6ff82bcfcd0f035b44d80f
aam3 <amarsteller95@gmail.com>
2024-10-19 16:27:51 -0400

    added react bootstrap formatting with dropdown

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,34 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/'
 import './index.scss'
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faClose,
-  faBars
-} from '@fortawesome/free-solid-svg-icons'
+// import { useState } from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import {
+//   faClose,
+//   faBars
+// } from '@fortawesome/free-solid-svg-icons'
 
 const Layout = () => {
-
-  const [showNav, setShowNav] = useState(false);
   
   return (
     <div className="App">
-      <div className={`sidebar-div ${showNav ? 'mobile-show' : ''}`}>
-        <Sidebar />
-      </div>      
-      <FontAwesomeIcon 
-          onClick={() => setShowNav(true)}
-          icon={faBars}
-          color="#ffd700"
-          size="3x"
-          className={`hamburger-icon ${showNav ? 'mobile-show' : ''}`} />
-      <FontAwesomeIcon 
-          onClick={() => setShowNav(false)}
-          icon={faClose}
-          color="#d6d5d5"
-          size="3x"
-          className={`close-icon ${showNav ? 'mobile-show' : ''}`} />      
+      <Sidebar />
       <div className="page">
         <Outlet />
       </div>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,39 +1,69 @@
 import './index.scss'
-import { useState } from 'react'
+// import { useState } from 'react'
 // import LogoS from '../../assets/images/logo-s.png'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { Link, NavLink } from 'react-router-dom'
+// import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/Logo.svg'
 
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Sidebar = () => {
-  const [showNav, setShowNav] = useState(false);
-
   return (
-    <div className={`nav-bar ${showNav ? 'mobile-show' : ''}`}>
-      <nav>
-        <div className="nav-content">
-          <Link to='/' className="logo-link" onClick={() => setShowNav(false)}>
-          <img className='signature-logo' src={LogoS} alt='logo'/>
-          </Link>
-          <NavLink 
-            activeclassname="active"
-            className="about-link"
-            to="/about"
-            onClick={() => setTimeout(() => setShowNav(false), 100)}>About</NavLink>
-          <NavLink
-            activeclassname="active"
-            className="publications-link"
-            to="/publications"
-            onClick={() => setShowNav(false)}>Publications</NavLink>
-          <NavLink
-            activeclassname="active"
-            className="resume-link"
-            to="/resume"
-            onClick={() => setShowNav(false)}>Resume</NavLink>       
-        </div>             
-      </nav>
-    </div>
-  )
-}
+    <>
+    <Navbar expand="lg" className="custom-navbar p-4" bg="dark" fixed="top" collapseOnSelect="true">
+      {/* <Container > */}
+        <Navbar.Brand href="/"><img className='signature-logo' src={LogoS} alt='logo'/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav-content d-flex">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/publications">Publications</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      {/* </Container> */}
+    </Navbar>
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
+
+
+
+
+// const Sidebar = () => {
+//   const [showNav, setShowNav] = useState(false);
+
+//   return (
+//     <div className={`nav-bar ${showNav ? 'mobile-show' : ''}`}>
+//       <nav>
+//         <div className="nav-content">
+//           <Link to='/' className="logo-link" onClick={() => setShowNav(false)}>
+//           <img className='signature-logo' src={LogoS} alt='logo'/>
+//           </Link>
+//           <NavLink 
+//             activeclassname="active"
+//             className="about-link"
+//             to="/about"
+//             onClick={() => setTimeout(() => setShowNav(false), 100)}>About</NavLink>
+//           <NavLink
+//             activeclassname="active"
+//             className="publications-link"
+//             to="/publications"
+//             onClick={() => setShowNav(false)}>Publications</NavLink>
+//           <NavLink
+//             activeclassname="active"
+//             className="resume-link"
+//             to="/resume"
+//             onClick={() => setShowNav(false)}>Resume</NavLink>       
+//         </div>             
+//       </nav>
+//     </div>
+//   )
+// }
+
+// export default Sidebar
