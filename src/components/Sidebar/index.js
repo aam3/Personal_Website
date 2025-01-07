87a4ad95edd1db7d3bde26aeb2bc6687b3d7f7d3
aam3 <aam3@alumni.princeton.edu>
2025-01-06 23:28:16 -0500

    fixing build error

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,4 @@
 import './index.scss'
-import LogoS from '../../assets/images/Logo.svg'
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
