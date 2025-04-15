a40a75f85fb66b3759049a43d7962505af707bc8
aam3 <aam3@alumni.princeton.edu>
2025-04-15 13:52:44 -0400

    changed home icon color and moved text up

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,10 +15,10 @@ const Home = () => {
                     <a href={resumePDF} className="resume-button" target="_blank" rel="noopener noreferrer">Resumé</a>
                     <div className="social-icons">
                         <a href="https://github.com/aam3" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faGithub} style={{ color: "#444345" }} />
+                            <FontAwesomeIcon icon={faGithub} style={{ color: "#000000" }} />
                         </a>
                         <a href="https://www.linkedin.com/in/alisabeth-m-21273b117/" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faLinkedin} style={{ color: "#444345" }} />
+                            <FontAwesomeIcon icon={faLinkedin} style={{ color: "#000000" }} />
                         </a>
                     </div>
                 </div>
